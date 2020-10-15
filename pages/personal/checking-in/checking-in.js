2563fd5e2a0ccb0ed5a48704558db7b7be5e9fe4
Ammoxy <15976506547@qq.com>
2020-10-15 17:47:11 +0800

    update

--- pages/personal/checking-in/checking-in.js
+++ pages/personal/checking-in/checking-in.js
@@ -1,18 +1,77 @@
 // pages/personal/checking-in/checking-in.js
+var Lunar = require('../../../utils/lunar')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    nowTime: ''
+    nowTime: '',
+    daysColor: [{
+      month: 'current', // 要标记的日期所属月份，有效值有prev（上个月）,current（当前月），next（下个月）
+      day: new Date().getDate(), // 要标记的日期
+      color: 'white', // 白色
+      background: "#2a9f93" //日期单元格的颜色
+    }], // 日历
+    show: false,
+    showTime: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.currentTime()
+    this.currentTime();
+    // 当前时间
+    let time = new Date();
+    let year = time.getFullYear();
+    let month = time.getMonth() + 1;
+    let date = time.getDate();
+    let gregorian = Lunar.getdateTime(year, month, date); // 星期
+    let lunar = Lunar.toLunar(year, month, date); // 农历
+  },
+
+  showCaledar() {
+    var self = this;
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      show: true
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+
+  closeCaledar() {
+    var self = this;
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        show: false
+      })
+    }.bind(this), 200)
   },
 
   currentTime() {
@@ -23,9 +82,9 @@ Page({
     var self = this;
     let hh = new Date().getHours();
     let mf =
-      new Date().getMinutes() < 10
-        ? "0" + new Date().getMinutes()
-        : new Date().getMinutes();
+      new Date().getMinutes() < 10 ?
+      "0" + new Date().getMinutes() :
+      new Date().getMinutes();
     let ss = new Date().getSeconds() < 10 ? "0" + new Date().getSeconds() : new Date().getSeconds();
     self.setData({
       nowTime: hh + ":" + mf + ":" + ss
