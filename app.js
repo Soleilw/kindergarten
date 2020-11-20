1db99da739c96fc0f55df81506f45c294e3b60bb
Ammoxy <15976506547@qq.com>
2020-11-20 16:19:27 +0800

    update

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ App({
     let app = this;
     console.log('getXConfig');
     wx.request({
-      url: 'https://gong.fengniaotuangou.cn/api/user/config?school=kindergarten&version=1.0.5',
+      url: 'https://gong.fengniaotuangou.cn/api/user/config?school=kindergarten&version=1.0.6',
       method:'GET',
       success:function(res){
         console.log(res.data.data);
